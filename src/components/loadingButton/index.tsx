2a445131b90bd92e01bcb331e434faa09c693c3a
Asfaq-Mohamed <asfaq.mohamed@aspiresys.com>
2025-01-02 18:16:01 +0530

    loader UI adjustment

--- src/components/loadingButton/index.tsx
+++ src/components/loadingButton/index.tsx
@@ -1,16 +1,16 @@
 import { IconButton, CircularProgress } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
-
+// MuiCircularProgress-svg
 const LoadingButton = ({ onClick, isLoading }: { onClick: () => void; isLoading: boolean }) => {
     return (
         <>
             {isLoading ? (
                 <CircularProgress
                     size={20}
-                    style={{ color: 'secondary.main', position: 'absolute' }}
+                    sx={{ color: 'secondary.main' }}
                 />
             ) : (
-                <IconButton onClick={onClick} disabled={!isLoading} aria-label="send">
+                <IconButton onClick={onClick} aria-label="send">
                     <SendIcon sx={{
                         color: 'white',
                         backgroundColor: 'secondary.main',
