2f560c579d5d4dd041dd6717641fa640353f4347
Achu1ahm <asfaqahmed128@gmail.com>
2025-01-03 12:22:05 +0530

    Theme Adjustments

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chat from './app/chat';
 import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 import theme from './styles/theme/theme';
 import AppLayout from './components/layout';
 
@@ -8,6 +9,7 @@ import AppLayout from './components/layout';
 function App() {
   return (
     <ThemeProvider theme={theme}>
+       <CssBaseline />
       <AppLayout>
       <Chat />
       </AppLayout>
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -9,6 +9,52 @@ const theme = createTheme({
             main: '#b002c7',
         },
     },
+    components: {
+        MuiCssBaseline: {
+          styleOverrides: {
+            h1: {
+              fontSize: '1.5em',
+             
+            },
+            h2: {
+              fontSize: '1.35em',
+              
+            },
+            h3: {
+              fontSize: '1.25em',
+             
+            },
+            h4: {
+              fontSize: '1.15em',
+              
+            },
+            h5: {
+              fontSize: '1.05em',
+            },
+            h6: {
+              fontSize: '1em',
+            },
+            p: {
+              fontSize: '1em',
+            },
+            li: {
+              fontSize: '1em',
+              lineHeight: 1.6,
+              margin: '0.5rem 0',
+            },
+            code: {
+                fontSize: '1em',
+            }
+          },
+        },
+        MuiSvgIcon: {
+          styleOverrides: {
+            root: {
+              fontSize: '2.2rem',
+            },
+          },
+        },
+      }
 });
 
 export default theme;
